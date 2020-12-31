1df165ab6354cd17b4ff97fd0e744d54c4d4aac2
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-12-30 20:37:09 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,14 +5,13 @@ var survivalTime=0;
 var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
-var x;
+var x = 0;
 var y;
 function preload(){
   monkey_running = loadAnimation("images/sprite_0.png","images/sprite_1.png","images/sprite_2.png","images/sprite_3.png","images/sprite_4.png","images/sprite_5.png","images/sprite_6.png","images/sprite_7.png","images/sprite_8.png");
   bananaImage = loadImage("images/banana.png");
   obstacleImage = loadImage("images/obstacle.png");
   jungleImage = loadImage("images/jungle.jpg");
- 
 }
 
 function setup() {
@@ -20,10 +19,10 @@ function setup() {
    jungle = createSprite(displayWidth*4, displayHeight);
   jungle.addImage(jungleImage);
   jungle.scale = 3;
-  monkey = createSprite(80, height-75, 20, 20);
+  monkey = createSprite(80, displayHeight-75, 20, 20);
   monkey.addAnimation("running",monkey_running);
   monkey.scale = 0.1;
-  ground = createSprite(70, height-50, displayWidth-20, 10);
+  ground = createSprite(70, displayHeight-50, displayWidth-20, 10);
   console.log(ground.x);
   obstaclesGroup = createGroup();
   foodGroup = createGroup();
@@ -33,11 +32,12 @@ function setup() {
 
 
 function draw() {
-background(0);
-camera.position.x = displayWidth+x;
-camera.position.y = displayHeight/2;
-  if(gameState===PLAY){
-    // monkey jumps if space is pressed
+background("turquoise");
+if(gameState===PLAY){
+  x = x+4;
+  camera.position.x = displayWidth+x;
+  camera.position.y = displayHeight/2;
+  monkey.x = camera.position.x - 250;
   if(keyDown("space")|| touches.length>0) {
         monkey.velocityY = -12;
     touches=[];
@@ -46,24 +46,13 @@ camera.position.y = displayHeight/2;
   if(obstaclesGroup.isTouching(monkey)){
     gameState=END;
   }
-   // obstacle.collide(ground);
   spawnObstacles();
   spawnBananas();
   ground.visible = true;
   }
-  
-  else if(gameState===END){
-    ground.velocityX = 0;
-    monkey.velocityY = 0;
-    jungle.velocityX = 0;
-     obstaclesGroup.setLifetimeEach(-1);
-    foodGroup.setLifetimeEach(-1);
-     
-     obstaclesGroup.setVelocityXEach(0);
-     foodGroup.setVelocityXEach(0);  
-  
-   
-  }
+  /*if(frameCount>865){
+    gameState = END;
+  }*/
   monkey.collide(ground);
   ground.visible=false;
   drawSprites();
@@ -77,27 +66,29 @@ function survivalTimeScore(){
     survivalTime = survivalTime+1; 
     foodGroup.destroyEach();
   }
-  text("Survival time:"+ survivalTime,displayWidth/2,displayHeight-800);
+  text("Survival time:"+ survivalTime,displayWidth/2+x,displayHeight-800);
 }
 
 function spawnObstacles(){
-   camera.position.x = displayWidth - 20;
-   camera.position.y = displayHeight/2;
-   obstacle = createSprite(500, height-70, 10, 40);
-   obstacle.addImage(obstacleImage);
-   obstacle.scale = 0.1;
-   //add each obstacle to the group
-    obstaclesGroup.add(obstacle);
+   if(frameCount%200===0){
+    obstacle = createSprite(500, height-70, 10, 40);
+    obstacle.addImage(obstacleImage);
+    obstacle.scale = 0.1;
+    obstacle.x = camera.position.x+100;
+    //add each obstacle to the group
+     obstaclesGroup.add(obstacle);
+   }
   }
 
 function spawnBananas(){
-    camera.position.x = Math.round(random(120,300));
-    banana = createSprite(width-40, height-85, 20, 20);
-    banana.y = Math.round(random(120,300));
-    banana.addImage(bananaImage);
-    banana.scale = 0.05;
-    //adjust the depth
-    banana.depth = monkey.depth;
-    monkey.depth = monkey.depth + 1;
-    foodGroup.add(banana);
+    if(frameCount%120===0){
+      banana = createSprite(width-40, height-85, 20, 20);
+      banana.y = Math.round(random(displayHeight/2-50,displayHeight/2+50));
+      banana.addImage(bananaImage);
+      banana.scale = 0.05;
+      banana.x = camera.position.x+100;
+      banana.depth = monkey.depth;
+      monkey.depth = monkey.depth + 1;
+      foodGroup.add(banana);
+    }
   }
